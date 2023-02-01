e8a5099db89cf40444f5ea31156b7a7d721616eb
frederikpyt <frederikpyt@gmail.com>
2023-02-01 08:39:11 +0100

    #1 - Setup theme, app icon and splash screen

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -9,9 +9,18 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
-        colorScheme: ColorScheme.fromSwatch(
-          accentColor: const Color(0xFF13B9FF),
+        appBarTheme: const AppBarTheme(color: Color(0xFF212121)),
+        colorScheme: const ColorScheme.dark(
+          primary: Color(0xFF212121),
+          onPrimary: Color(0xFFcfb39d),
+          secondary: Color(0xFF212121),
+          onSecondary: Color(0xFFcfb39d),
+          surface: Color(0xFF212121),
+          onSurface: Color(0xFFa68f7d),
+          tertiary: Color(0xFF212121),
+          onTertiary: Color(0xFFcfb39d),
+          background: Color(0xFF212121),
+          onBackground: Color(0xFFcfb39d),
         ),
       ),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
