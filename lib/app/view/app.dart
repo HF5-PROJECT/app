00c1608a93e2a34eb778841a5151bd2f5123abad
Kristian Binau <kristian@binau.me>
2023-02-07 14:24:42 +0100

    #4 - Start of login page

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:overnites/counter/counter.dart';
+import 'package:overnites/login/login.dart';
 import 'package:overnites/l10n/l10n.dart';
 
 class App extends StatelessWidget {
@@ -10,12 +10,13 @@ class App extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
         appBarTheme: const AppBarTheme(color: Color(0xFF212121)),
-        colorScheme: const ColorScheme.dark(
+        colorScheme: const ColorScheme.light(
+          brightness: Brightness.light,
           primary: Color(0xFF212121),
           onPrimary: Color(0xFFcfb39d),
           secondary: Color(0xFF212121),
           onSecondary: Color(0xFFcfb39d),
-          surface: Color(0xFF212121),
+          surface: Color(0xFF313030),
           onSurface: Color(0xFFa68f7d),
           tertiary: Color(0xFF212121),
           onTertiary: Color(0xFFcfb39d),
@@ -25,7 +26,7 @@ class App extends StatelessWidget {
       ),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      home: const CounterPage(),
+      home: const LoginPage(),
     );
   }
 }
---
+++ lib/login/view/login_page.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+import 'package:overnites/l10n/l10n.dart';
+import 'package:overnites/login/login.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => LoginCubit(),
+      child: const LoginView(),
+    );
+  }
+}
+
+class LoginView extends StatelessWidget {
+  const LoginView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+    return Scaffold(
+      appBar: AppBar(title: Text(l10n.counterAppBarTitle)),
+      body: Center(
+          child: ElevatedButton(
+        onPressed: () {
+          loginModal(context);
+        },
+        child: const Text(
+          'Login',
+          style: TextStyle(fontSize: 24),
+        ),
+      )),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () => context.read<LoginCubit>().increment(),
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(height: 8),
+          FloatingActionButton(
+            onPressed: () => context.read<LoginCubit>().decrement(),
+            child: const Icon(Icons.remove),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<dynamic> loginModal(BuildContext context) {
+    return showMaterialModalBottomSheet(
+      expand: false,
+      context: context,
+      backgroundColor: Color.fromARGB(255, 255, 0, 0),
+      builder: (context) => Material(
+        child: SafeArea(
+      top: false,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          ListTile(
+            title: Text('Edit'),
+            leading: Icon(Icons.edit),
+            onTap: () => Navigator.of(context).pop(),
+          ),
+          ListTile(
+            title: Text('Copy'),
+            leading: Icon(Icons.content_copy),
+            onTap: () => Navigator.of(context).pop(),
+          ),
+          ListTile(
+            title: Text('Cut'),
+            leading: Icon(Icons.content_cut),
+            onTap: () => Navigator.of(context).pop(),
+          ),
+          ListTile(
+            title: Text('Move'),
+            leading: Icon(Icons.folder_open),
+            onTap: () => Navigator.of(context).pop(),
+          ),
+          ListTile(
+            title: Text('Delete'),
+            leading: Icon(Icons.delete),
+            onTap: () => Navigator.of(context).pop(),
+          )
+        ],
+      ),
+    )),
+    );
+  }
+}
